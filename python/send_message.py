7e526f478fe0e83cc5e03edb56e7d3432c0ec117
brandon <brandonpeck13@gmail.com>
2020-01-02 20:06:07 -0800

    updates

--- python/send_message.py
+++ python/send_message.py
@@ -1,9 +1,10 @@
 from twilio.rest import Client
 def send_message(action='sit up', to=''):
 	account_sid = 'AC025dbe29a3dc777a9433d15edbfbb9ce'
+	auth_token = '6f31f4a09df1eff674c7a78a2c5e89c8'
 	client = Client(account_sid, auth_token)
 
-        payload = 'The action '+ action + ' was detected.'
+	payload = 'The action '+ action + ' was detected.'
 
 	message = client.messages \
 			.create(
--- python/stream_darknet2.py
+++ python/stream_darknet2.py
@@ -1,3 +1,4 @@
+from send_message import send_message
 from ctypes import *
 import math
 import random
@@ -37,6 +38,73 @@ def cvDrawBoxes(detections, img):
 netMain = None
 metaMain = None
 altNames = None
+
+
+important_objs = ['person']
+
+class Box:
+    def __init__(self, x1,y1,x2,y2 ):
+        self.bottom = y2
+        self.top = y1
+        self.left = x1
+        self.right = x2
+class Point:
+    def __init__(self, x,y):
+        self.x = x
+        self.y = y
+
+
+def sitting_up_detection(detections):
+    #{'person': Box, 'bed':Box}
+    if len(detections) > 2: return False
+    if 'person' not in detections or 'bed' not in detections: return False
+    exit_left, exit_right, exit_top, exit_bottom = False, False, False, False
+    person_pos = detections['person']
+    #bed_pos = detections['manual_bed']
+    bed_pos = detections['bed']
+    delta = 0 # threshold of person bed overlap before detection
+    if person_pos.x - delta < bed_pos.left:
+        exit_left = True
+    elif person_pos.x + delta > bed_pos.right:
+        exit_right = True
+    elif person_pos.y - delta > bed_pos.bottom:
+        exit_bottom = True
+    elif person_pos.y + delta < bed_pos.top:
+        exit_top = True
+    return exit_top or exit_bottom or exit_left or exit_right
+
+def img_select_event(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        bed_point_list.append((x,y))
+        cv2.circle(param,(x,y),10,(255,0,0),-1)
+        print(x,y)
+
+def draw_bed(frame):
+    global bed_point_list
+    bed_point_list = []
+    while True:
+        cv2.namedWindow('image')
+        cv2.setMouseCallback('image', img_select_event, param=frame)
+        cv2.imshow('image', frame)
+        k = cv2.waitKey(20) & 0xFF
+        if k == 27:
+            break
+    return bed_point_list
+
+
+def avg_box_pos(detected_obj_history):
+    avg_box = Box(0,0,0,0)
+    for b in detected_obj_history:
+        avg_box.left += b.left/len(detected_obj_history)
+        avg_box.right += b.right/len(detected_obj_history)
+        avg_box.bottom += b.bottom/len(detected_obj_history)
+        avg_box.top += b.top/len(detected_obj_history)
+
+    avg_box.left = int(avg_box.left)
+    avg_box.right = int(avg_box.right)
+    avg_box.bottom = int(avg_box.bottom)
+    avg_box.top = int(avg_box.top)
+    return avg_box
 
 
 def YOLO():
@@ -91,6 +159,14 @@ def YOLO():
     # Create an image we reuse for each detect
     darknet_image = darknet.make_image(darknet.network_width(netMain),
                                     darknet.network_height(netMain),3)
+    initialize_bed = True
+    bed_point_list = []
+    detected_obj_history = []
+    frame_avg_num = 10
+    b = Box(0, 0, 0, 0)
+    detections = {}
+    situp_timer = time.time()  # starts when situp detected and reset after 10 mins
+    first_time_detected = True
     while True:
         prev_time = time.time()
         ret, frame_read = cap.read()
@@ -102,9 +178,30 @@ def YOLO():
 
         darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
 
-        detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+        frame_data = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
+
+        for content in frame_data:
+            key = content[0].decode('utf-8')
+            if key == 'person':
+                obj_coords = [int(c) for c in content[2]]
+                new_b = Box(obj_coords[0] - int(obj_coords[2]/2), obj_coords[1] - int(obj_coords[3]/2), obj_coords[0]+int(obj_coords[2]/2), obj_coords[1]+int(obj_coords[3]/2))
+                detected_obj_history.append(new_b)
+                if len(detected_obj_history) > frame_avg_num:
+                    detected_obj_history = detected_obj_history[1:]
+                b = avg_box_pos(detected_obj_history)
+        person_point = Point(int((b.right +b.left)/2), int((b.top+b.bottom)/2))
+        detections['person'] = person_point
+      #  cv2.rectangle(frame, (b.left, b.top, b.right, b.bottom), (0, 255, 0), 2)
+        is_sitting_up = sitting_up_detection(detections)
+        print(is_sitting_up)
+        situp_time_elapsed = time.time() - situp_timer
+        if is_sitting_up and (situp_time_elapsed >= 600 or first_time_detected):
+            send_message()
+            situp_timer = time.time()
+            first_time_detected = False
         image = cvDrawBoxes(detections, frame_resized)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        cv2.circle(image,(person_point.x,person_point.y),10,(255,0,0),-1)
         print(1/(time.time()-prev_time))
         cv2.imshow('Demo', image)
         cv2.waitKey(3)
